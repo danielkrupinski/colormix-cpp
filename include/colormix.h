057cd9436d947ed51b65e5b846e72b7f1f459928
Daniel Krupiński <danielkrupinski@outlook.com>
2018-04-14 19:56:55 +0200

    Add color ids

--- include/colormix.h
+++ include/colormix.h
@@ -2,12 +2,12 @@
 #define COLORMIX_H
 
 enum class Color {
-    red,
-    blue,
-    yellow,
-    purple,
-    orange,
-    green
+    red = 1,
+    blue = 2,
+    yellow = 4,
+    purple = 3,
+    orange = 5,
+    green = 6
 };
 
 #endif // COLORMIX_H
