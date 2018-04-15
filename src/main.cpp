6f12be98b605a28c0393eaea7b4f4af6ecfa578b
Daniel Krupiński <danielkrupinski@outlook.com>
2018-04-15 10:17:36 +0200

    Remove Color c3

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,6 @@ int main()
     Color c1 {static_cast<Color>(inputColor("first"))};
     Color c2 {static_cast<Color>(inputColor("second"))};
 
-    Color c3 {c1+c2};
     std::cout << "You mixed " << c1 << " and " << c2 << " and got " << c1+c2 << std::endl;
 
     return 0;
