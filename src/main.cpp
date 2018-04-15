cbba183b79ba5bf55a81c4a7efc9663dbd0b11e1
Daniel Krupiński <danielkrupinski@outlook.com>
2018-04-15 09:18:20 +0200

    Add colors objects

--- src/main.cpp
+++ src/main.cpp
@@ -16,5 +16,11 @@ int main()
                  "3. Yellow\n";
     int b {0};
     std::cin >> a;
+
+    Color c1 {static_cast<Color>(a)};
+    Color c2 {static_cast<Color>(b)};
+
+    Color c3 {c1+c2};
+
     return 0;
 }
