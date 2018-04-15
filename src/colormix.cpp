6d9a95e6e6ad8374c5d59faaaba17fc457153535
Daniel Krupiński <danielkrupinski@outlook.com>
2018-04-15 09:28:11 +0200

    Update Color::getColor() and clean-up main.cpp

--- src/colormix.cpp
+++ src/colormix.cpp
@@ -1,1 +1,12 @@
 #include "../include/colormix.h"
+
+int Color::getColor()
+{
+    std::cout << "Select first color:\n"
+                 "1. Red\n"
+                 "2. Blue\n"
+                 "3. Yellow\n";
+    int a {0};
+    std::cin >> a;
+    return a;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -3,22 +3,8 @@
 
 int main()
 {
-    std::cout << "Select first color:\n"
-                 "1. Red\n"
-                 "2. Blue\n"
-                 "3. Yellow\n";
-    int a {0};
-    std::cin >> a;
-
-    std::cout << "Select second color:\n"
-                 "1. Red\n"
-                 "2. Blue\n"
-                 "3. Yellow\n";
-    int b {0};
-    std::cin >> a;
-
-    Color c1 {static_cast<Color>(a)};
-    Color c2 {static_cast<Color>(b)};
+    Color c1 {static_cast<Color>(Color::getColor())};
+    Color c2 {static_cast<Color>(Color::getColor())};
 
     Color c3 {c1+c2};
 
