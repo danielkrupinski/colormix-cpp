2d42769edc14f9b58ca448c2172d749a3d53df3c
Daniel Krupiński <danielkrupinski@outlook.com>
2018-04-15 09:55:00 +0200

    Overload operator<<

--- src/colormix.cpp
+++ src/colormix.cpp
@@ -16,3 +16,28 @@ constexpr Color operator+(Color a, Color b)
 {
     return static_cast<Color>(static_cast<int>(a)+static_cast<int>(b));
 }
+
+std::ostream& operator<<(std::ostream& os, Color color)
+{
+    switch (color) {
+    case Color::red:
+        os << "Red";
+        break;
+    case Color::blue:
+        os << "Blue";
+        break;
+    case Color::purple:
+        os << "Purple";
+        break;
+    case Color::yellow:
+        os << "Yellow";
+        break;
+    case Color::orange:
+        os << "Orange";
+        break;
+    case Color::green:
+        os << "Green";
+        break;
+    }
+    return os;
+}
