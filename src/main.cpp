d0a3eb6a42f165c490b349024c294927824f3ebd
Daniel Krupiński <danielkrupinski@outlook.com>
2018-04-15 09:14:19 +0200

    Add color selection

--- src/main.cpp
+++ src/main.cpp
@@ -3,5 +3,18 @@
 
 int main()
 {
+    std::cout << "Select first color:\n"
+                 "1. Red\n"
+                 "2. Blue\n"
+                 "3. Yellow\n";
+    int a {0};
+    std::cin >> a;
+
+    std::cout << "Select second color:\n"
+                 "1. Red\n"
+                 "2. Blue\n"
+                 "3. Yellow\n";
+    int b {0};
+    std::cin >> a;
     return 0;
 }
