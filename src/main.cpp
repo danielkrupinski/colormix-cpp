42b9144575ec93c80d646c874f68dcde077d8b79
Daniel Krupiński <danielkrupinski@outlook.com>
2018-04-16 19:18:25 +0200

    Clear console after every input

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,10 @@
 int main()
 {
     Color c1 {static_cast<Color>(inputColor("first"))};
+    clearScreen();
     Color c2 {static_cast<Color>(inputColor("second"))};
-
+    clearScreen();
+    
     if (c1==c2)
         std::cout << "You mix " << c1 << " and " << c2 << " and get " << c1 << std::endl;
     else
