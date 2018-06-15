4d0141ad937a81665bbd80be88bf9e505562f59f
Daniel Krupiński <danielkrupinski@outlook.com>
2018-06-15 17:39:55 +0200

    Remove unnecessary static_cast - casting is done in inputColor(const std::string&&)

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,9 @@
 
 int main()
 {
-    Color color1{static_cast<Color>(inputColor("first"))};
+    Color color1{(inputColor("first")};
     clearScreen();
-    Color color2{static_cast<Color>(inputColor("second"))};
+    Color color2{(inputColor("second")};
     clearScreen();
 
     std::cout << "You mix " << color1 << " and " << color2 << " and get " << color1 + color2 << std::endl;
