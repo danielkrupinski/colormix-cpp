b97464eb3cfd9d49d0d2f7eebb9d06f1173a642a
Daniel Krupiński <danielkrupinski@outlook.com>
2018-06-15 17:18:52 +0200

    Remove check if picked colors are both the same as it is done in constexpr Color operator+(Color a, Color b)

--- src/main.cpp
+++ src/main.cpp
@@ -6,11 +6,8 @@ int main()
     clearScreen();
     Color c2 {static_cast<Color>(inputColor("second"))};
     clearScreen();
-    
-    if (c1==c2)
-        std::cout << "You mix " << c1 << " and " << c2 << " and get " << c1 << std::endl;
-    else
-        std::cout << "You mix " << c1 << " and " << c2 << " and get " << c1+c2 << std::endl;
+
+    std::cout << "You mix " << c1 << " and " << c2 << " and get " << c1 + c2 << std::endl;
 
     return 0;
 }
